374c054db45b1f5115138d2b39cd05b3e4887870
Maelle Vance <maellevance@gmail.com>
2017-01-16 21:32:28 -0800

    metadata and score print

--- scraper.py
+++ scraper.py
@@ -157,7 +157,11 @@ if __name__ == '__main__':
         html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
     listings = extract_data_listings(doc)
-    for listing in listings[:5]:
+    for listing in listings:
         metadata = extract_restaurant_metadata(listing)
+        inspection_row = listing.find_all(is_inspection_row)
         score_data = extract_score_data(listing)
-        print(score_data)
+        metadata.update(score_data)
+        for key, value in metadata.items():
+            print(key, value)
+        print()
